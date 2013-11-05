2b56f31b4a8664f7efc35eb6de6e772ff42f30c2
konsolenheld <konsolenheld@localhost.localdomain>
2013-11-05 09:32:47 +0100

    making an own module for IndexDict

--- pymeasure/case.py
+++ pymeasure/case.py
@@ -1,57 +1,5 @@
-from collections import OrderedDict
+from indexdict import IndexDict
 import time
-
-
-class IndexDict(object):
-
-    def __init__(self):
-        self._odict = OrderedDict()
-
-    def __iter__(self):
-        return iter(self._odict.values())
-
-    def __len__(self):
-        return len(self._odict)
-
-    def __getitem__(self, key):
-        try:
-            return self._odict[key]
-        except KeyError:
-            try:
-                key = self._odict.keys()[key]
-                return self._odict[key]
-            except:
-                raise KeyError
-
-    def __setitem__(self, key, item):
-        if type(key) is not str:
-            raise KeyError('key must be str')
-        else:
-            self._odict[key] = item
-
-    def __delitem__(self, key):
-        try:
-            del self._odict[key]
-        except KeyError:
-            try:
-                key = self._odict.keys()[key]
-                del self._odict[key]
-            except:
-                raise KeyError
-
-    def __repr__(self):
-
-        repr_str = self.__class__.__name__
-
-        repr_str += '['
-        for index, key in enumerate(self._odict.keys()):
-            repr_str += str(index) + ': \'' + key + '\', '
-        repr_str = repr_str[:-2] + ']'
-
-        return repr_str
-
-    def keys(self):
-        return self._odict.keys()
 
 
 class Channel(object):
