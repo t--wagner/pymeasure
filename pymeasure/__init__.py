f7c09685f68d987ad37ffdda1b5ad2f8606caa9c
t--wagner <konsolenheld@localhost.localdomain>
2014-05-22 10:00:58 +0200

    filetools

--- pymeasure/__init__.py
+++ pymeasure/__init__.py
@@ -4,5 +4,6 @@ import instruments
 import backends
 from case import Instrument, Rack
 from liveplot import LiveGraphTk, Dataplot1d, Dataplot2d
-from measurment import Measurment1d, Measurment2d
-from sweep import LinearSweep, TimeSweep
+from measurment import *
+from sweep import *
+from filetools import *
