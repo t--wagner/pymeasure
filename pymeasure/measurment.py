5562c9138d154690ed6f3b22f065983600acf69e
t--wagner <konsolenheld@localhost.localdomain>
2014-05-23 09:05:53 +0200

    Measurment module

--- pymeasure/measurment.py
+++ pymeasure/measurment.py
@@ -4,22 +4,48 @@ import abc
 from threading import Thread, Event
 
 
-class Measurment1d(Thread):
+class MeasurmentError(Exception):
+    pass
+
+
+class MeasurenmtBase():
     __metaclass__ = abc.ABCMeta
 
-    def __init__(self, sweep=None):
+    def __init__(self, pickle=True):
 
-        Thread.__init__(self)
-
-        self._sweep = sweep
+        self._thread = None
 
         self._data = []
+        self._pickle = pickle
 
         self._pause_event = Event()
         self._pause_event.set()
         self._stop_event = Event()
 
-        self.step = []
+        self._current_step = []
+
+    @property
+    def pickle(self):
+        return self._pickle
+
+    @pickle.setter
+    def pickle(self, boolean):
+        self._pickle = boolean
+
+    @property
+    def current_step(self):
+        return self._current_step
+
+    @property
+    def data(self):
+        return self._data
+
+    @property
+    def is_running(self):
+        if self._thread is None:
+            return False
+        else:
+            return self._thread.is_alive()
 
     def pause(self):
 
@@ -27,6 +53,35 @@ class Measurment1d(Thread):
             self._pause_event.clear()
         else:
             self._pause_event.set()
+
+    def stop(self):
+        self._stop_event.set()
+
+    def start(self):
+
+        if self._thread is not None:
+            if self._thread.is_alive():
+                raise MeasurmentError('Measurment is running.')
+
+        self._thread = Thread(target=self._run)
+        self._thread.start()
+
+    @abc.abstractmethod
+    def _acquire(self):
+        pass
+
+    @abc.abstractmethod
+    def _run(self):
+        pass
+
+
+class Measurment1d(MeasurenmtBase):
+
+    def __init__(self, sweep=None):
+
+        MeasurenmtBase.__init__(self)
+
+        self._sweep = sweep
 
     @property
     def sweep(self):
@@ -36,53 +91,33 @@ class Measurment1d(Thread):
     def sweep(self, sweep):
         self._sweep = sweep
 
-    @property
-    def data(self):
-        return self._data
-
-    @abc.abstractmethod
-    def _acquire(self):
-        pass
-
-    def run(self):
+    def _run(self):
 
         for step in self._sweep:
 
-            self.step = step
+            self._current_step = step
 
             self._acquire()
 
             self._pause_event.wait()
 
             if self._stop_event.is_set():
+                self._stop_event.clear()
                 break
 
 
-class Measurment2d(Thread):
-    __metaclass__ = abc.ABCMeta
+class Measurment2d(MeasurenmtBase):
 
     def __init__(self, sweep0=None, sweep1=None):
 
-        Thread.__init__(self)
+        MeasurenmtBase.__init__(self)
 
         self._sweep0 = sweep0
         self._sweep1 = sweep1
 
-        self._data = []
+        self._hold_event = Event()
 
-        self._pause_event = Event()
-        self._pause_event.set()
-        self._break_event = Event()
-        self._stop_event = Event()
-
-        self.step = []
-
-    def pause(self):
-
-        if self._pause_event.is_set():
-            self._pause_event.clear()
-        else:
-            self._pause_event.set()
+        self._current_step = [[], []]
 
     @property
     def sweep0(self):
@@ -100,25 +135,30 @@ class Measurment2d(Thread):
     def sweep1(self, sweep):
         self._sweep1 = sweep
 
-    @property
-    def data(self):
-        return self._data
+    def hold(self):
+        self._hold_event.set()
 
-    @abc.abstractmethod
-    def _acquire(self):
-        pass
-
-    def run(self):
+    def _run(self):
 
         for step0 in self._sweep0:
 
+            self._current_step[0] = step0
+
             for step1 in self._sweep1:
 
-                self.step = step
+                self._current_step[1] = step1
 
                 self._acquire()
 
                 self._pause_event.wait()
 
+                if self._stop_event.is_set():
+                    break
+
             if self._stop_event.is_set():
+                self._stop_event.clear()
+                break
+
+            if self._hold_event.is_set():
+                self._hold_event.clear()
                 break
--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*
 
 import pymeasure as pym
+from itertools import izip as zip
 
 # Create instruments
 foo = pym.instruments.FooInstrument('foo')
@@ -13,13 +14,15 @@ sample['vxx'] = foo['sin']
 sample['vxy'] = foo['cos']
 
 
-class MyMeasurment(pym.Measurment1d):
+class MyMeasurment(pym.Measurment2d):
 
     def _acquire(self):
-        print self.step
-
+        print self._current_step
 
 if __name__ == '__main__':
-    meas1d = MyMeasurment()
-    meas1d.sweep = pym.LinearSweep(foo['out1'], 0, 10, 101, 0.250)
-    meas1d.start()
+    meas2d = MyMeasurment()
+    sweep01 = pym.LinearSweep(foo['out1'], 0, 10, 11, 0.250)
+    sweep02 = pym.LinearSweep(foo['out1'], 10, 0, 11, 0.250)
+    meas2d.sweep0 = zip(sweep01, sweep02)
+    meas2d.sweep1 = pym.LinearSweep(foo['out1'], 0, 10, 11, 0.250)
+    meas2d.start()
