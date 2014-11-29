401a4b51f947d827ac72464467a18af71cb8307b
unknown <wagner@Thomson.nano.uni-hannover.de>
2014-11-29 18:00:44 +0100

    try except instrument imports

--- pymeasure/__init__.py
+++ pymeasure/__init__.py
@@ -1,12 +1,1 @@
 # -*- coding: utf-8 -*
-
-#import pymeasure.backends
-import pymeasure.instruments
-from pymeasure.case import Instrument, Rack
-#from pymeasure.data import Data
-from pymeasure.filetools import (create_directory, create_file, index_str,
-    cut_filetype, DirectoryIndexer, BasenameIndexer)
-from pymeasure.liveplot import LiveGraphTk, Dataplot1d, Dataplot2d
-from pymeasure.loop import Loop, LoopNested
-from pymeasure.measurment import Measurment
-from pymeasure.sweep import SweepSteps, SweepLinear, SweepTime, SweepZip
--- pymeasure/instruments/instruments_list.py
+++ pymeasure/instruments/instruments_list.py
@@ -1,11 +1,21 @@
 # -*- coding: utf-8 -*-
 
 from pymeasure.instruments.foo_instrument import FooInstrument
-from pymeasure.instruments.ad5791dac import Ad5791Dac
-from pymeasure.instruments.adwin_pro2_adc import AdwinPro2ADC
-from pymeasure.instruments.adwin_pro2_daq import AdwinPro2Daq
-from pymeasure.instruments.ronny_valve import RonnyValve
-from pymeasure.instruments.egg7260_lock_in_amplifier import Egg7260LockInAmplifier
-from pymeasure.instruments.keithley2400_sourcemeter import Keithley2400SourceMeter
-from pymeasure.instruments.oxford_ips import QxfordIPS
-from pymeasure.instruments.oxford_ps_120 import QxfordPS120
+
+#Visa Imports
+try:
+    from pymeasure.instruments.ad5791dac import Ad5791Dac
+    from pymeasure.instruments.ronny_valve import RonnyValve
+    from pymeasure.instruments.egg7260_lock_in_amplifier import Egg7260LockInAmplifier
+    from pymeasure.instruments.keithley2400_sourcemeter import Keithley2400SourceMeter
+    from pymeasure.instruments.oxford_ips import QxfordIPS
+    from pymeasure.instruments.oxford_ps_120 import QxfordPS120
+except ImportError:
+    pass
+
+#ADwin Imports
+try:
+    from pymeasure.instruments.adwin_pro2_adc import AdwinPro2ADC
+    from pymeasure.instruments.adwin_pro2_daq import AdwinPro2Daq
+except:
+    pass
