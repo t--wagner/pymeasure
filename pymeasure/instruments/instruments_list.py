be654a39d265b83197ea50a7b7d4648d1b7692a4
Timo Wagner <timo.wag@gmail.com>
2015-01-29 12:33:00 +0100

    egg5219_lock_in

--- pymeasure/instruments/instruments_list.py
+++ pymeasure/instruments/instruments_list.py
@@ -7,6 +7,7 @@ try:
     from pymeasure.instruments.ad5791dac import Ad5791Dac
     from pymeasure.instruments.ronny_valve import RonnyValve
     from pymeasure.instruments.egg7260_lock_in_amplifier import Egg7260LockInAmplifier
+    from pymeasure.instruments.egg5210_lock_in_amplifier import Egg5210LockInAmplifier
     from pymeasure.instruments.keithley2400_sourcemeter import Keithley2400SourceMeter
     from pymeasure.instruments.oxford_ips import QxfordIPS
     from pymeasure.instruments.oxford_ps_120 import QxfordPS120
