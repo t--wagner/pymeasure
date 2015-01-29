6ba3c95d8ab043fcbfcac4ade4beed0b0775e792
Timo Wagner <timo.wag@gmail.com>
2015-01-29 16:09:45 +0100

    instrument list

--- pymeasure/instruments/instruments_list.py
+++ pymeasure/instruments/instruments_list.py
@@ -9,9 +9,9 @@ try:
     from pymeasure.instruments.egg7260_lock_in_amplifier import Egg7260LockInAmplifier
     from pymeasure.instruments.egg5210_lock_in_amplifier import Egg5210LockInAmplifier
     from pymeasure.instruments.keithley2400_sourcemeter import Keithley2400SourceMeter
+    from pymeasure.instruments.keithley2000_multimeter import Keithley2000Multimeter
     from pymeasure.instruments.oxford_ips import QxfordIPS
     from pymeasure.instruments.oxford_ps_120 import QxfordPS120
-    from pymeasure.instruments.keithley2000_multimeter import Keithley2000Multimeter
     #  Keithley2000MultiMeter von Felix hinzugefügt     
     
       
